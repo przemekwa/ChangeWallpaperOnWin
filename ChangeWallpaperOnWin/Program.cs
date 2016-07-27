86c32477749bf806fbe1c9efad1b081dabd95c6b
127356 <przemyslaw.walkowski@bzwbk.pl>
2016-07-27 11:13:28 +0200

    Refactoring

--- ChangeWallpaperOnWin/Program.cs
+++ ChangeWallpaperOnWin/Program.cs
@@ -8,8 +8,57 @@ namespace ChangeWallpaperOnWin
 {
     class Program
     {
+        public static readonly string[] WallpaperRegisterKeys =
+        {
+            "Software", "Microsoft", "Windows", "CurrentVersion",
+            "Policies", "System"
+        };
+
+        public static readonly string[] WallpaperDeleteKeys = {"Wallpaper", "WallpaperStyle", "NoDispScrSavPage"};
+
+        public static readonly string[] CompositionRegisterKeys =
+        {
+            "Software", "Microsoft", "Windows", "CurrentVersion",
+            "Policies", "Explorer"
+        };
+
+        public static readonly string[] CompositionDeleteKeys = {"NoThemeTab,"};
+
         static void Main(string[] args)
         {
+            Console.WriteLine("--== Change Wallpaper On Win 7 ==--");
+
+            var rgisterOperations = new RgisterOperations();
+
+            var wallPaperResult =
+                TryChangeRegister(()=>rgisterOperations.DeleteValues(WallpaperRegisterKeys, WallpaperDeleteKeys));
+
+            var compositionResult =
+                TryChangeRegister(() => rgisterOperations.DeleteValues(CompositionRegisterKeys, CompositionDeleteKeys));
+
+            Console.WriteLine(wallPaperResult
+                ? "Rejestr został wyszczyszony. Można zmienić tapetę."
+                : "Nie udało się wyszczyści rejestru. Tapety nie będzie można zmienić.");
+
+            Console.WriteLine(compositionResult
+                ? "Rejestr został wyszczyszony. Można zmienić kompozycję."
+                : "Nie udało się wyszczyści rejestru. Tapety nie będzie można zmienić.");
+
+
+            Console.Read();
+        }
+
+        static bool TryChangeRegister(Func<bool> changeRegisterFunc)
+        {
+            try
+            {
+                return changeRegisterFunc.Invoke();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error {0}", e.Message);
+                return false;
+            }
         }
     }
 }
---
+++ ChangeWallpaperOnWin/RgisterOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace ChangeWallpaperOnWin
+{
+    internal class RgisterOperations
+    {
+        public bool DeleteValues(string[] path, string[] valuesToDelete)
+        {
+            RegistryKey key = null;
+
+            path.ToList().ForEach(x =>
+            {
+                key = CurrentUserFinder(x, key);
+            });
+
+            if (key == null) return false;
+
+            var values = key.GetValueNames();
+
+            values.ToList().ForEach(x =>
+            {
+                if (valuesToDelete.Contains(x))
+                {
+                    key.DeleteValue(x);
+                }
+            });
+
+            return true;
+        }
+
+        private static RegistryKey CurrentUserFinder(string name, RegistryKey key = null)
+        {
+            return key == null ? Registry.CurrentUser.OpenSubKey(name, RegistryKeyPermissionCheck.ReadWriteSubTree) : key.OpenSubKey(name, true);
+        }
+    }
+}
